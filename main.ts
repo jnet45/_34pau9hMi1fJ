0cdd41498d474134c33dc46804ebc2b9284f8ef5
jnet45 <81304762+jnet45@users.noreply.github.com>
2022-07-11 18:24:10 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,19 @@
 radio.onReceivedNumber(function (receivedNumber) {
     basic.clearScreen()
     if (receivedNumber == 1) {
-        pins.analogWritePin(AnalogPin.P8, 256)
+        pins.analogWritePin(AnalogPin.P12, 256)
         pins.analogWritePin(AnalogPin.P16, 0)
         led.plot(4, 2)
     } else if (receivedNumber == 2) {
-        pins.analogWritePin(AnalogPin.P8, 0)
+        pins.analogWritePin(AnalogPin.P12, 0)
         pins.analogWritePin(AnalogPin.P16, 256)
         led.plot(0, 2)
     } else if (receivedNumber == 3) {
-        pins.analogWritePin(AnalogPin.P8, 256)
+        pins.analogWritePin(AnalogPin.P12, 256)
         pins.analogWritePin(AnalogPin.P16, 256)
         led.plot(2, 4)
     } else if (receivedNumber == 0) {
-        pins.analogWritePin(AnalogPin.P8, 0)
+        pins.analogWritePin(AnalogPin.P12, 0)
         pins.analogWritePin(AnalogPin.P16, 0)
         led.plot(2, 2)
     } else {
