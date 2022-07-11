9dc35e502c0cd00a64de86259f64921b536e580e
jnet45 <81304762+jnet45@users.noreply.github.com>
2022-07-11 23:32:31 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,16 @@
 radio.onReceivedNumber(function (receivedNumber) {
     basic.clearScreen()
     if (receivedNumber == 1) {
-        pins.analogWritePin(AnalogPin.P12, 256)
+        pins.analogWritePin(AnalogPin.P12, 512)
         pins.analogWritePin(AnalogPin.P16, 0)
         led.plot(4, 2)
     } else if (receivedNumber == 2) {
         pins.analogWritePin(AnalogPin.P12, 0)
-        pins.analogWritePin(AnalogPin.P16, 256)
+        pins.analogWritePin(AnalogPin.P16, 512)
         led.plot(0, 2)
     } else if (receivedNumber == 3) {
-        pins.analogWritePin(AnalogPin.P12, 256)
-        pins.analogWritePin(AnalogPin.P16, 256)
+        pins.analogWritePin(AnalogPin.P12, 512)
+        pins.analogWritePin(AnalogPin.P16, 512)
         led.plot(2, 4)
     } else if (receivedNumber == 0) {
         pins.analogWritePin(AnalogPin.P12, 0)
